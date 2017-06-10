df29c42ad1e3f1556a011aff372c729c4ceed59e
Alexander Khalamirenko <isalliok@gmail.com>
2017-06-11 00:14:06 +0300

    Initial commit

---
+++ src/com/ac143/khalamirenko/Controller.java
@@ -0,0 +1,37 @@
+package com.ac143.khalamirenko;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void storagePressed(ActionEvent actionEvent) {
+    }
+
+    public void backspacePressed(ActionEvent actionEvent) {
+
+    }
+
+    public void operation2Pressed(ActionEvent actionEvent) {
+
+    }
+
+    public void numberPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void operation1Pressed(ActionEvent actionEvent) {
+
+    }
+
+    public void equalPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void clearLastInputPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void clearCalculationsPressed(ActionEvent actionEvent) {
+
+    }
+}
